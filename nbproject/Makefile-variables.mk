ea50681e6da90adeca3a4d6448aad6b2130a4d37
Diego De Leonardis <diegowazzaap@hotmail.it>
2015-10-12 19:51:50 +0200

    First Commit

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -15,13 +15,13 @@ CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=html-c.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/html-c.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=html-c
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/html-c
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/html-c
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=html-c.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/html-c.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/html-c.tar
 #
 # include compiler specific variables
 #
